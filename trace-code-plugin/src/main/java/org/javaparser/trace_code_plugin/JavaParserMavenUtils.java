6b9b25044abcd19a622a2861f6fe9dba14384734
Danny van Bruggen <hexagonaal@gmail.com>
2017-09-03 15:39:31 +0200

    Convert to annotations

---
+++ trace-code-plugin/src/main/java/org/javaparser/trace_code_plugin/JavaParserMavenUtils.java
@@ -0,0 +1,27 @@
+package org.javaparser.trace_code_plugin;
+
+import org.apache.maven.plugin.logging.Log;
+
+/**
+ * Things to make JavaParser and Maven interact better
+ */
+public class JavaParserMavenUtils {
+    public static void makeJavaParserLogToMavenOutput(Log log) {
+        com.github.javaparser.utils.Log.setAdapter(new com.github.javaparser.utils.Log.Adapter() {
+            @Override
+            public void info(String message) {
+                log.info(message);
+            }
+
+            @Override
+            public void trace(String message) {
+                log.debug(message);
+            }
+
+            @Override
+            public void error(Throwable throwable, String f) {
+                log.error(f, throwable);
+            }
+        });
+    }
+}
--- trace-code-plugin/src/main/java/org/javaparser/trace_code_plugin/TraceCodePluginMojo.java
+++ trace-code-plugin/src/main/java/org/javaparser/trace_code_plugin/TraceCodePluginMojo.java
@@ -6,10 +6,13 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.utils.Log;
 import com.github.javaparser.utils.SourceRoot;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,62 +22,63 @@ import static com.github.javaparser.utils.CodeGenerationUtils.f;
 /**
  * This plugin is a sample for building your own plugins. It takes a directory of source code and adds a trace line to
  * each method.
- *
- * @goal trace-code
- * @phase process-sources
  */
+@Mojo(name = "trace-code", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class TraceCodePluginMojo extends AbstractMojo {
     /**
      * Location for the source files with added trace lines.
-     *
-     * @parameter
-     * @required
      */
+    @Parameter(required = true)
     private File sourceDirectory;
 
     /**
      * Location for the source files with added trace lines.
-     *
-     * @parameter default-value="${project.build.directory}/generated-sources/trace-code"
-     * @required
      */
+    @Parameter(required = true, defaultValue = "${project.build.directory}/generated-sources/trace-code")
     private File destinationDirectory;
 
     @Override
     public void execute() throws MojoExecutionException {
-        Log log = getLog();
-        addTraceCode(log, sourceDirectory, destinationDirectory);
+        JavaParserMavenUtils.makeJavaParserLogToMavenOutput(getLog());
+        addTraceCodeToAllFiles(sourceDirectory, destinationDirectory);
     }
 
-    private void addTraceCode(Log log, File sourceDirectory, File destinationDirectory) throws MojoExecutionException {
-        log.error("Adding tracing info to source from " + sourceDirectory + " and storing in " + destinationDirectory);
-
-        SourceRoot source = new SourceRoot(sourceDirectory.toPath());
+    private void addTraceCodeToAllFiles(File sourceDirectory, File destinationDirectory) throws MojoExecutionException {
+        final SourceRoot source = new SourceRoot(sourceDirectory.toPath());
         try {
             for (ParseResult<CompilationUnit> parseResult : source.tryToParse()) {
+                // Only deal with files without parse errors
                 if (parseResult.isSuccessful()) {
-                    addTracing(parseResult.getResult().get());
+                    parseResult.getResult().ifPresent(cu -> {
+                        // Make the plugin a little noisier
+                        cu.getStorage().ifPresent(storage -> Log.info(f("Processing %s...", storage.getFileName())));
+                        // Do the actual logic
+                        addTraceCodeToCompilationUnit(cu);
+                    });
                 }
             }
+            // Everything parsed with sourceroot is kept in a cache so that it can be saved to disk in 1 line:
             source.saveAll(destinationDirectory.toPath());
         } catch (IOException e) {
             throw new MojoExecutionException("Error reading from source directory", e);
         }
-
-        if (!destinationDirectory.exists()) {
-            destinationDirectory.mkdirs();
-        }
     }
 
-    private void addTracing(CompilationUnit compilationUnit) {
+    private void addTraceCodeToCompilationUnit(CompilationUnit compilationUnit) {
+        // Use a visitor to easily find methods
         compilationUnit.accept(new VoidVisitorAdapter<Void>() {
             @Override
             public void visit(MethodDeclaration n, Void arg) {
+                // Take the method body if it exists, and insert the trace statement. 
                 n.getBody().ifPresent(b -> b.getStatements().add(0, traceLine(n.getNameAsString())));
+                // In case there are methods somewhere inside this method, visit children:
                 super.visit(n, arg);
             }
 
             private Statement traceLine(String methodName) {
+                /* Creates a small AST of the code in the string.
+                "f" is a shortcut to String.format.
+                 */
                 return JavaParser.parseStatement(f("System.out.println(\"Entering %s\");", methodName));
             }
         }, null);
